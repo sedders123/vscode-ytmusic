4431ac16e689a79095cd33da8a51bdcda6b40876
James Seden Smith <sedders123@gmail.com>
2024-12-18 17:52:16 +0000

    Change Cache namespae to ensure reauth needed when extension updated

--- src/youtubeMusic.ts
+++ src/youtubeMusic.ts
@@ -31,7 +31,7 @@ export default class YouTubeMusic {
   private _socket: Socket;
 
   public constructor(context: ExtensionContext) {
-    this._codeCache = new Cache(context, "ytMusic");
+    this._codeCache = new Cache(context, "vscode-ytmusic");
     const authCode = this._codeCache.get("authCode") as string;
     if (authCode) {
       this.initSocket(authCode);
